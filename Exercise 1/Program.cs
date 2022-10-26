ecb7ca681f9bc78b46a64728e686d337453902e9
duyndse <duyndse@gmail.com>
2022-10-26 08:13:19 +0700

    init commit

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise_1;
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        int x = 0;
+        bool isValid = true;
+        try
+        {
+            Console.WriteLine("Enter a score: ");
+            x = int.Parse(Console.ReadLine() ?? String.Empty);
+        }
+        catch (FormatException)
+        {
+            isValid = false;
+            Console.WriteLine("Invalid format score");
+        }
+        catch (OverflowException)
+        {
+            isValid = false;
+            Console.WriteLine("Score is too large");
+        }
+        finally
+        {
+            if (isValid)
+                Console.WriteLine("Score is: " + x);
+        }
+    }
+}
---
+++ Exercise 2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercise_2;
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        var list = new List<string>();
+        try
+        {
+            var streamReader = new StreamReader("TextFile.txt");
+            while (!streamReader.EndOfStream)
+            {
+                var line = streamReader.ReadLine();
+                if (line == null) continue;
+                list.Add(line);
+            }
+
+            var i = 1;
+            foreach (var line in list)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(line);
+                i++;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
